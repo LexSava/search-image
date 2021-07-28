ff33bc4cdd22fb10bae564b9c19fec6e31afc4ae
LexSava <lexsava@gmail.com>
2021-07-28 22:01:32 +0300

    feat: add card and save event in store

--- client/src/components/Main/Main.tsx
+++ client/src/components/Main/Main.tsx
@@ -1,11 +1,59 @@
 import React from 'react';
 import './Main.scss';
-import { Container } from 'react-bootstrap';
+import { Container, Button, Card, Form, FormControl } from 'react-bootstrap';
+import { BsChevronLeft, BsChevronRight } from 'react-icons/bs';
+import Store from '../../store/Store';
 
 function Main() {
   return (
     <Container className="p-0 ps-3">
-      No images here. Whould you try to search for anything else?
+      <Container className="p-0 d-flex justify-content-end pe-3">
+        <Button variant="outline-dark" className="btn-back">
+          <BsChevronLeft className="btn-back-icon" />
+          Back
+        </Button>
+        <Button variant="dark" disabled className="btn-pages">
+          Page 1 of 10
+        </Button>
+        <Button variant="outline-dark" className="btn-forward">
+          Forward
+          <BsChevronRight className="btn-forward-icon" />
+        </Button>
+      </Container>
+
+      <Container className="p-0 mt-5">
+        <Card style={{ width: '18rem' }}>
+          <Card.Img
+            variant="top"
+            src="holder.js/100px180"
+            className="card-img"
+          />
+          <Card.Body>
+            {/* <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text> */}
+
+            <Form inline className="w-100 ">
+              <Button variant="dark" onClick={() => Store.getCounter()}>
+                Bookmark it!
+              </Button>
+              <FormControl
+                type="text"
+                placeholder="Some tags?"
+                className="mt-3"
+                // value={inputText}
+                // onChange={changeHandle}
+                // onInput={changeHandle}
+                // onKeyPress={keyPressHandler}
+              />
+            </Form>
+          </Card.Body>
+        </Card>
+      </Container>
+
+      {/* No images here. Whould you try to search for anything else? */}
     </Container>
   );
 }
--- client/src/components/Seacrh/Seacrh.tsx
+++ client/src/components/Seacrh/Seacrh.tsx
@@ -2,9 +2,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './Seacrh.scss';
 import React, { useState, useEffect } from 'react';
 import { Container, Form, FormControl, Button } from 'react-bootstrap';
+import Store from '../../store/Store';
+// import { observer } from 'mobx-react-lite';
 
-function Seacrh() {
+const Seacrh = () => {
   // const [searchText, setSearchText] = useState<string>("Search");
+  const [inputText, setInputText] = useState<string>('');
+  const [searchText, setSearchText] = useState<string>('');
+
+  const changeHandle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(event.target.value);
+    Store.getSearchQuery(event.target.value);
+  };
+
+  const keyPressHandler = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+    }
+  };
+
+  // const enteredSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   Store.getSearchQuery(inputText);
+  // };
+
+  // console.log(inputText);
 
   return (
     <Container className="search-block w-100 p-3 text-dark d-flex justify-content-between ">
@@ -14,20 +35,14 @@ function Seacrh() {
           placeholder="Find images"
           className=""
           autoFocus
-          // value={inputText}
+          value={inputText}
           // onChange={changeHandle}
-          // onInput={changeHandle}
-          // onKeyPress={keyPressHandler}
+          onInput={changeHandle}
+          onKeyPress={keyPressHandler}
         />
-        <Button
-          variant="dark"
-          // onClick={inputLog}
-        >
-          Search
-        </Button>
       </Form>
     </Container>
   );
-}
+};
 
 export default Seacrh;
